3085b17c16a983d75bd1a813405971f1c2c50bfe
nigelbess <nigelbess@gmail.com>
2018-12-05 14:47:40 -0800

    commiting someone elses changes

--- JoystickOnlyTraining.m
+++ JoystickOnlyTraining.m
@@ -26,16 +26,17 @@ port = 'COM3';
                 lastReading = 1;
                 r.LogJoy(currReading,currSide,GetSecs());
                 e.playReward();
-                %e.deactivateServos();
-                startLickWindow = GetSecs;    
-                %Wait for mouse to lick
-                while GetSecs - startLickWindow < .5
-                    if(e.isLicking())
-                        r.LogLick(GetSecs-startLickWindow);
-                        break;
-                    end
-                end
-                e.giveWater(.05);
+%                 e.deactivateServos();
+                pause(0.1)
+%                 startLickWindow = GetSecs;    
+%                 %Wait for mouse to lick
+%                 while GetSecs - startLickWindow < .5
+%                     if(e.isLicking())
+%                         r.LogLick(GetSecs-startLickWindow);
+%                         break;
+%                     end
+%                 end
+                e.giveWater(.03);
                 currSide = roll();
                 e.closeServos();
                 e.openSide(currSide);
@@ -44,7 +45,7 @@ port = 'COM3';
         else
             lastReading = 0;
         end
-        e.refillWater(.03h)
+        e.refillWater(.01)
         pause(.005)
     end
     
